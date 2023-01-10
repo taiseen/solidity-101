db60c2df94af59e492d9ee806e895e522230ac2e
taiseen <taiseen.cse@gmail.com>
2023-01-10 18:28:27 +0600

    feat: hello world start

---
+++ 01_helloWorld.sol
@@ -0,0 +1,13 @@
+// SPDX-License Identifier: GPL-3.0
+pragma solidity >=0.5.0 <0.9.0;
+
+contract 01_helloWorld {
+
+    string public hello = "Hello World!";
+    
+    function get() public view returns(string memory){
+        return hello;
+    }
+}
+
+    
