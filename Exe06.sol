2d326277808a9964991c228ed0953af3b68c34cb
taiseen <taiseen.cse@gmail.com>
2023-02-28 15:26:13 +0600

    refactor: function input

--- Exe06.sol
+++ Exe06.sol
@@ -1,11 +1,25 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity >=0.5.0 <0.9.0;
 
-contract exercise05 {
-    uint public number;
+contract exercise06 {
+    uint number = 100;
+    int negativeNum = -100;
+    string name = "Sam";
 
-    // constructor is only run once and at the beginning
-    constructor(uint count) {
-        number = count;
+    function getnegativeNum() public view returns (int) {
+        return negativeNum;
+    }
+
+    function getNumber() public view returns (uint) {
+        return number;
+    }
+
+    function getName() public view returns (string memory) {
+        return name;
+    }
+
+    function changeNegativeNum(int x, int y, int z) public {
+        int total = x + y + z;
+        negativeNum = negativeNum + total;
     }
 }
